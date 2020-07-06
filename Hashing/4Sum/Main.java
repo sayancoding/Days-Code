0da754695170908976f119d0e3792c6ba3c1ed80
SAYAN MAITY <sayanmaity007@gmail.com>
2020-07-06 10:05:31 +0530

    #4 ,sum

---
+++ Hashing/4Sum/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Main {
+  static void fourSum(int[] nums, int target) {
+    HashMap<Integer, int[]> map = new HashMap<>();
+    //two sum and store to hash
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        int sum = nums[i] + nums[j];
+        int[] pair = new int[2];
+        pair[0] = i;
+        pair[1] = j;
+        map.put(sum, pair);
+      }
+    }
+    // check the inverse of that two sum from hash
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        int x = target - (nums[i] + nums[j]);
+        if (map.containsKey(x)) {
+          if (i != map.get(x)[0] && i != map.get(x)[1] && j != map.get(x)[0] && j != map.get(x)[1])
+            System.out.println(i + " " + j + " " + map.get(x)[0] + " " + map.get(x)[1]);
+            return;
+        }
+      }
+    }
+
+  }
+  public static void main(String[] args) {
+    int[] nums = new int[] { 10, 2, 3, 4, 5, 9, 7, 8 };
+    int target = 23;
+    fourSum(nums, target);
+  }
+}
