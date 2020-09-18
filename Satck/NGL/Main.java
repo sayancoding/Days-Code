04327c292e77575d717cb5b4b468959357a95aea
SAYAN MAITY <sayanmaity007@gmail.com>
2020-09-18 08:57:25 +0530

    NGL #stack

---
+++ Satck/NGL/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Main {
+  public static int[] NGR(int[] arr) {
+    int[] result = new int[arr.length];
+    Stack<Integer> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (st.isEmpty()) {
+        result[i] = -1;
+        st.add(arr[i]);
+      } else {
+        while (!st.isEmpty()) {
+          if (st.peek() <= arr[i] && st.size() > 1) {
+            st.pop();
+          } else if (st.peek() <= arr[i] && st.size() == 1) {
+            st.pop();
+            result[i] = -1;
+            st.add(arr[i]);
+            break;
+          } else if (st.peek() > arr[i]) {
+            result[i] = st.peek();
+            st.add(arr[i]);
+            break;
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  static void print(int[] arr) {
+    for (int el : arr) {
+      System.out.print(el + " ");
+    }
+  }
+
+  public static void main(String[] args) {
+    // int[] arr = new int[] { 1, 3, 2, 4};
+    int[] arr = new int[] { 1, 3, 0, 0, 1, 2, 4 };
+    print(NGR(arr));
+  }
+}
