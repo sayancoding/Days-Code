637a759ca27194e8ec26e7fa77e962ce97a68ff2
SAYAN MAITY <sayanmaity007@gmail.com>
2020-09-22 13:47:50 +0530

    #rainwatertrapping

---
+++ Satck/minEleStack/Main.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+class Main{
+  // static int minElement(int[] arr){
+  // }
+  public static void main(String[] args) {
+    int[] arr = new int[] {18,19,29,16,15};
+    // minElement(arr);
+  }
+}
