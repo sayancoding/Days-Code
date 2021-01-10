5037911105442a150af23671a83717862d1ad37a
sayancoding <sayanmaity007@gmail.com>
2021-01-10 08:55:12 +0530

    #max sum subsequence

---
+++ Dynamic Programming/maxSumIncreasedSubsequence/maxSumSequence.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class maxSumSequence{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(maxSum(arr));
+
+    }
+
+    static int maxSum(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int[] dp = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int temp_max = arr[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i]) {
+                    temp_max = Math.max(dp[j] + arr[i], temp_max);
+                }
+            }
+            dp[i] = temp_max;
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+}
